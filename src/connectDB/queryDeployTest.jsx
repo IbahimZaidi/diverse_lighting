c015e3bf397079aa28b198f0edf93f073dbb23d0
brahim <brahim@gmail.com>
2024-05-05 17:48:20 +0100

    transfrom from the online db to localDB to make the process more fast

--- src/connectDB/queryDeployTest.jsx
+++ src/connectDB/queryDeployTest.jsx
@@ -4,14 +4,20 @@ import mysql from "mysql2/promise";
 // https://jsonplaceholder.typicode.com/
 
 export const queryDeployTest = async ({ query, values }) => {
+  // const dbConn = await mysql.createConnection({
+  //   host: "sql11.freesqldatabase.com", //
+  //   database: "sql11704007",
+  //   user: "sql11704007",
+  //   password: "cJi19iep1U",
+  //   // port: 3306,
+  // });
   const dbConn = await mysql.createConnection({
-    host: "sql11.freesqldatabase.com", //
-    database: "sql11704007",
-    user: "sql11704007",
-    password: "cJi19iep1U",
+    host: "localhost", //
+    database: "diverseLighte",
+    user: "root",
+    password: "",
     // port: 3306,
   });
-
   try {
     const [results] = await dbConn.execute(query, values);
 
